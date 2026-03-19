f3be235a4e543f41169b839a69ecb61d805b940e
kwppwk <kevinwpower@gmail.com>
2026-03-19 19:10:06 +0800

    Update streamlit_app.py
    remove stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop
+#s t.stop
 
 
 ingredients_list = st.multiselect(
